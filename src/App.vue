d6ff15ae517071b52a7617e2e589b128b11cb8ad
whtsky <whtsky@gmail.com>
2016-02-04 02:01:55 +0800

    Add bg image && define font-family

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view></router-view>
+  <div id="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
@@ -9,6 +11,12 @@ export default {
 
 <style>
 body {
-  font-family: Helvetica, sans-serif;
+  font-family: -apple-system,
+    "Helvetica Neue", "Arial",
+    "PingFang SC", "Hiragino Sans GB", "STHeiti", "Microsoft YaHei", "Microsoft JhengHei",
+    "Source Han Sans SC", "Noto Sans CJK SC", "Source Han Sans CN", "Noto Sans SC",
+    "Source Han Sans TC", "Noto Sans CJK TC", "WenQuanYi Micro Hei", SimSun, sans-serif;
+  background: url('./assets/bg.jpg') repeat;
 }
+  
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="search">
   </div>
 </template>
 
